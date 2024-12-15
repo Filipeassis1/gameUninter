459522f93b2e27d967df89551432e9aa368cc582
Filipe Assis <filipeassisr@gmail.com>
2024-12-14 23:04:35 -0300

    Add classes and Menu

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-import pygame
+from code.Game import Game
 
-print('Start Setup')
-pygame.init()
-window = pygame.display.set_mode(size=(600, 480))
-print('End Setup')
-
-print('Loop Start')
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            print('Quitting..')
-            pygame.quit()  #close
-            quit()  #end
+game = Game()
+game.run()
